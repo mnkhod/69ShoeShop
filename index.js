762f71d55277ebc092c10fcb9219e9e3bad9794d
mnkhod <mnkhod.dev@gmail.com>
2021-08-24 19:01:48 +0800

    Added Process ENV Port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path = require('path');
 // });
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('assets'))
 // app.use(connectLiveReload());
