47fc0e6da871f28bb1a346f78a56dc855e32949e
mnkhod <mnkhod.dev@gmail.com>
2021-08-24 19:12:22 +0800

    Fixed Main Shoe Responsiveness

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path = require('path');
 // });
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3500
 
 app.use(express.static('assets'))
 // app.use(connectLiveReload());
